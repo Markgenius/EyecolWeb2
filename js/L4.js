ee542b3448a438b35034b5e1c255954b64a48664
Mark <18697098+Markgenius@users.noreply.github.com>
2023-06-01 17:06:54 +0800

    L4 mouse event

--- js/L4.js
+++ js/L4.js
@@ -170,8 +170,8 @@ function clickBG32_1(){
     buttonGroup33_5.setAttribute('style','none');
 }
 
-buttonGroup22_1.addEventListener('click',clickBG22_1);
-buttonGroup22_2.addEventListener('click',clickBG22_1);
+buttonGroup22_1.addEventListener('mouseup',clickBG22_1);
+buttonGroup22_2.addEventListener('mousedown',clickBG22_1);
 buttonGroup23_1.addEventListener('click',clickBG23_1);
 buttonGroup23_2.addEventListener('click',clickBG23_1);
 buttonGroup24_1.addEventListener('click',clickBG24_1);
