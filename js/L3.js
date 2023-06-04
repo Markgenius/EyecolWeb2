71092412c57b829780f95b61bb3cb3a72648751f
jimmy-yee <81695313+jimmy-yee@users.noreply.github.com>
2023-06-04 16:54:05 +0800

    edit L3

---
+++ js/L3.js
@@ -0,0 +1,277 @@
+var background_1 = document.querySelector(".background_image1");
+var option1 = document.querySelector(".option1");
+var option2 = document.querySelector(".option2");
+
+var background_2 = document.querySelector(".background_image2");
+var option3 = document.querySelector(".option3");
+var option4 = document.querySelector(".option4");
+
+var background_3 = document.querySelector(".background_image3");
+var option5 = document.querySelector(".option5");
+var option6 = document.querySelector(".option6");
+
+var background_25 = document.querySelector(".background_image25");
+var buttonGroup25_1 = document.querySelector(".button25_1");
+var buttonGroup25_2 = document.querySelector(".button25_2");
+
+var background_26 = document.querySelector(".background_image26");
+var buttonGroup26_0 = document.querySelector(".button26_0");
+var buttonGroup26_1 = document.querySelector(".button26_1");
+var buttonGroup26_2 = document.querySelector(".button26_2");
+var buttonGroup26_3 = document.querySelector(".button26_3");
+var buttonGroup26_4 = document.querySelector(".button26_4");
+
+var background_28 = document.querySelector(".background_image28");
+var buttonGroup28_1 = document.querySelector(".button28_1");
+var buttonGroup28_2 = document.querySelector(".button28_2");
+
+var background_29 = document.querySelector(".background_image29");
+var buttonGroup29_1 = document.querySelector(".button29_1");
+var buttonGroup29_2 = document.querySelector(".button29_2");
+var buttonGroup29_3 = document.querySelector(".button29_3");
+var buttonGroup29_4 = document.querySelector(".button29_4");
+var buttonGroup29_5 = document.querySelector(".button29_5");
+var buttonGroup29_6 = document.querySelector(".button29_6");
+var buttonGroup29_7 = document.querySelector(".button29_7");
+
+var background_30 = document.querySelector(".background_image30");
+var input_30 = document.querySelector(".input30");
+var buttonGroup30_1 = document.querySelector(".button30_1");
+
+var background_31 = document.querySelector(".background_image31");
+var buttonGroup31_1 = document.querySelector(".button31_1");
+var buttonGroup31_2 = document.querySelector(".button31_2");
+
+var background_32 = document.querySelector(".background_image32");
+var buttonGroup32_1 = document.querySelector(".button32_1");
+
+var background_33 = document.querySelector(".background_image33");
+var buttonGroup33_1 = document.querySelector(".button33_1");
+var buttonGroup33_2 = document.querySelector(".button33_2");
+var buttonGroup33_3 = document.querySelector(".button33_3");
+var buttonGroup33_4 = document.querySelector(".button33_4");
+var buttonGroup33_5 = document.querySelector(".button33_5");
+
+
+function clickBG22_1(){
+    background_1.setAttribute('style','visibility:hidden');
+    option1.setAttribute('style','visibility:hidden');
+    option2.setAttribute('style','visibility:hidden');
+
+    background_2.setAttribute('style','none');
+    option3.setAttribute('style','none');
+    option4.setAttribute('style','none');
+}
+
+function clickBG23_1(){
+    background_2.setAttribute('style','visibility:hidden');
+    option3.setAttribute('style','visibility:hidden');
+    option4.setAttribute('style','visibility:hidden');
+
+    background_3.setAttribute('style','none');
+    option5.setAttribute('style','none');
+    option6.setAttribute('style','none');
+}
+
+function clickBG24_1(){
+    background_24.setAttribute('style','visibility:hidden');
+    buttonGroup24_1.setAttribute('style','visibility:hidden');
+    buttonGroup24_2.setAttribute('style','visibility:hidden');
+
+    background_25.setAttribute('style','none');
+    buttonGroup25_1.setAttribute('style','none');
+    buttonGroup25_2.setAttribute('style','none');
+}
+
+function clickBG25_1(){
+    background_25.setAttribute('style','visibility:hidden');
+    buttonGroup25_1.setAttribute('style','visibility:hidden');
+    buttonGroup25_2.setAttribute('style','visibility:hidden');
+
+    background_26.setAttribute('style','none');
+    buttonGroup26_0.setAttribute('style','none');
+    buttonGroup26_1.setAttribute('style','none');
+    buttonGroup26_2.setAttribute('style','none');
+    buttonGroup26_3.setAttribute('style','none');
+    buttonGroup26_4.setAttribute('style','none');
+}
+
+function clickBG26_4(){
+    background_26.setAttribute('style','visibility:hidden');
+    buttonGroup26_0.setAttribute('style','visibility:hidden');
+    buttonGroup26_1.setAttribute('style','visibility:hidden');
+    buttonGroup26_2.setAttribute('style','visibility:hidden');
+    buttonGroup26_3.setAttribute('style','visibility:hidden');
+    buttonGroup26_4.setAttribute('style','visibility:hidden');
+
+    background_28.setAttribute('style','none');
+    buttonGroup28_1.setAttribute('style','none');
+    buttonGroup28_2.setAttribute('style','none');
+}
+
+function clickBG28_1(){
+    background_28.setAttribute('style','visibility:hidden');
+    buttonGroup28_1.setAttribute('style','visibility:hidden');
+    buttonGroup28_2.setAttribute('style','visibility:hidden');
+
+    background_29.setAttribute('style','none');
+    buttonGroup29_1.setAttribute('style','none');
+    buttonGroup29_2.setAttribute('style','none');
+    buttonGroup29_3.setAttribute('style','none');
+    buttonGroup29_4.setAttribute('style','none');
+    buttonGroup29_5.setAttribute('style','none');
+    buttonGroup29_6.setAttribute('style','none');
+    buttonGroup29_7.setAttribute('style','none');
+}
+
+function clickBG29_7(){
+    background_29.setAttribute('style','visibility:hidden');
+    buttonGroup29_1.setAttribute('style','visibility:hidden');
+    buttonGroup29_2.setAttribute('style','visibility:hidden');
+    buttonGroup29_3.setAttribute('style','visibility:hidden');
+    buttonGroup29_4.setAttribute('style','visibility:hidden');
+    buttonGroup29_5.setAttribute('style','visibility:hidden');
+    buttonGroup29_6.setAttribute('style','visibility:hidden');
+    buttonGroup29_7.setAttribute('style','visibility:hidden');
+
+    background_30.setAttribute('style','none');
+    input_30.setAttribute('style','none');
+    buttonGroup30_1.setAttribute('style','none');
+}
+
+function clickBG30_1(){
+    background_30.setAttribute('style','visibility:hidden');
+    input_30.setAttribute('style','visibility:hidden');
+    buttonGroup30_1.setAttribute('style','visibility:hidden');
+
+    background_31.setAttribute('style','none');
+    buttonGroup31_1.setAttribute('style','none');
+    buttonGroup31_2.setAttribute('style','none');
+}
+
+function clickBG31_1(){
+    background_31.setAttribute('style','visibility:hidden');
+    buttonGroup31_1.setAttribute('style','visibility:hidden');
+    buttonGroup31_2.setAttribute('style','visibility:hidden');
+
+    background_32.setAttribute('style','none');
+    buttonGroup32_1.setAttribute('style','none');
+}
+
+function clickBG32_1(){
+    background_32.setAttribute('style','visibility:hidden');
+    buttonGroup32_1.setAttribute('style','visibility:hidden');
+
+    background_33.setAttribute('style','none');
+    buttonGroup33_1.setAttribute('style','none');
+    buttonGroup33_2.setAttribute('style','none');
+    buttonGroup33_3.setAttribute('style','none');
+    buttonGroup33_4.setAttribute('style','none');
+    buttonGroup33_5.setAttribute('style','none');
+}
+
+function clickBG22_1_(){
+    option1.setAttribute('style','opacity: 0.5');
+}
+function clickBG22_2_(){
+    option2.setAttribute('style','opacity: 0.5');
+}
+function clickBG23_1_(){
+   option3.setAttribute('style','opacity: 0.5');
+}
+function clickBG23_2_(){
+    option4.setAttribute('style','opacity: 0.5');
+}
+function clickBG24_1_(){
+    buttonGroup24_1.setAttribute('style','opacity: 0.5');
+}
+function clickBG24_2_(){
+    buttonGroup24_2.setAttribute('style','opacity: 0.5');
+}
+function clickBG25_1_(){
+    buttonGroup25_1.setAttribute('style','opacity: 0.5');
+}
+function clickBG25_2_(){
+    buttonGroup25_2.setAttribute('style','opacity: 0.5');
+}
+function clickBG26_4_(){
+    buttonGroup26_4.setAttribute('style','opacity: 0.5');
+}
+function clickBG28_1_(){
+    buttonGroup28_1.setAttribute('style','opacity: 0.5');
+}
+function clickBG28_2_(){
+    buttonGroup28_2.setAttribute('style','opacity: 0.5');
+}
+function clickBG29_7_(){
+    buttonGroup29_7.setAttribute('style','opacity: 0.5');
+}
+function clickBG30_1_(){
+    buttonGroup30_1.setAttribute('style','opacity: 0.5');
+}
+function clickBG31_1_(){
+    buttonGroup31_1.setAttribute('style','opacity: 0.5');
+}
+function clickBG31_2_(){
+    buttonGroup31_2.setAttribute('style','opacity: 0.5');
+}
+function clickBG32_1_(){
+    buttonGroup32_1.setAttribute('style','opacity: 0.5');
+}
+
+
+option1.addEventListener('touchstart',clickBG22_1_);
+option1.addEventListener('touchend',clickBG22_1);
+
+option2.addEventListener('touchstart',clickBG22_2_);
+option2.addEventListener('touchend',clickBG22_1);
+
+option3.addEventListener('touchstart',clickBG23_1_);
+option3.addEventListener('touchend',clickBG23_1);
+
+option4.addEventListener('touchstart',clickBG23_2_);
+option4.addEventListener('touchend',clickBG23_1);
+
+buttonGroup24_1.addEventListener('touchstart',clickBG24_1_);
+buttonGroup24_1.addEventListener('touchend',clickBG24_1);
+
+buttonGroup24_2.addEventListener('touchstart',clickBG24_2_);
+buttonGroup24_2.addEventListener('touchend',clickBG24_1);
+
+buttonGroup25_1.addEventListener('touchstart',clickBG25_1_);
+buttonGroup25_1.addEventListener('touchend',clickBG25_1);
+
+buttonGroup25_2.addEventListener('touchstart',clickBG25_2_);
+buttonGroup25_2.addEventListener('touchend',clickBG25_1);
+
+buttonGroup26_4.addEventListener('touchstart',clickBG26_4_);
+buttonGroup26_4.addEventListener('touchend',clickBG26_4);
+
+buttonGroup28_1.addEventListener('touchstart',clickBG28_1_);
+buttonGroup28_1.addEventListener('touchend',clickBG28_1);
+
+buttonGroup28_2.addEventListener('touchstart',clickBG28_2_);
+buttonGroup28_2.addEventListener('touchend',clickBG28_1);
+
+buttonGroup29_7.addEventListener('touchstart',clickBG29_7_);
+buttonGroup29_7.addEventListener('touchend',clickBG29_7);
+
+buttonGroup30_1.addEventListener('touchstart',clickBG30_1_);
+buttonGroup30_1.addEventListener('touchend',clickBG30_1);
+
+buttonGroup31_1.addEventListener('touchstart',clickBG31_1_);
+buttonGroup31_1.addEventListener('touchend',clickBG31_1);
+
+buttonGroup31_2.addEventListener('touchstart',clickBG31_2_);
+buttonGroup31_2.addEventListener('touchend',clickBG31_1);
+
+buttonGroup32_1.addEventListener('touchstart',clickBG32_1_);
+buttonGroup32_1.addEventListener('touchend',clickBG32_1);
+
+
+// slider_1.oninput = function() {
+//     output1.innerHTML = this.value;
+// }
+// slider_2.oninput = function() {
+//     output2.innerHTML = this.value;
+// }
