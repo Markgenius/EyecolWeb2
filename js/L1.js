283735e707daa2bb33d8e9512364bbf8bff69f79
jimmy-yee <81695313+jimmy-yee@users.noreply.github.com>
2023-06-14 23:12:47 +0800

    edit L2

--- js/L1.js
+++ js/L1.js
@@ -9,15 +9,37 @@ function music(){
         playState = 1;
     }
 }
-document.addEventListener('click', music); 
+document.addEventListener('click', music);
+$(".bt1").on('touchstart',function(){
+    $(".bt1").attr('style','opacity: 0.5');
+}); 
+$(".bt2").on('touchstart',function(){
+    $(".bt2").attr('style','opacity: 0.5');
+}); 
+$("#a").on('touchstart',function(){
+    $("#a").attr('style','opacity: 0.5');
+}); 
+$("#b").on('touchstart',function(){
+    $("#b").attr('style','opacity: 0.5');
+}); 
+$(".bt4").on('touchstart',function(){
+    $(".bt4").attr('style','opacity: 0.5');
+}); 
+$("#c").on('touchstart',function(){
+    $("#c").attr('style','opacity: 0.5');
+});
+$("#d").on('touchstart',function(){
+    $("#d").attr('style','opacity: 0.5');
+});
 
-$(".bt1").click(function(){
+
+$(".bt1").on('touchend',function(){
     $(".bg1").attr('style','visibility:hidden');
     $(".bt1").attr('style','visibility:hidden');
     $(".bg2").attr('style','none');
     $(".bt2").attr('style','none');
 });
-$(".bt2").click(function(){
+$(".bt2").on('touchend',function(){
     $(".bg2").attr('style','visibility:hidden');
     $(".bt2").attr('style','visibility:hidden');
 
@@ -25,7 +47,7 @@ $(".bt2").click(function(){
     $(".dl3").attr('style','none');
     $(".bt3").attr('style','none');
 });
-$(".bt3").click(function(){
+$(".bt3").on('touchend',function(){
     $(".dl3").attr('style','visibility:hidden');
     $(".bt3").attr('style','visibility:hidden');
 
@@ -33,7 +55,7 @@ $(".bt3").click(function(){
     $(".bt4").attr('style','none');
     $(".input4").attr('style','visibility:none');
 });
-$(".bt4").click(function(){
+$(".bt4").on('touchend',function(){
     $(".dl4").attr('style','visibility:hidden');
     $(".bt4").attr('style','visibility:hidden');
     $(".input4").attr('style','visibility:hidden');
@@ -41,7 +63,7 @@ $(".bt4").click(function(){
     $(".dl5").attr('style','none');
     $(".bt5").attr('style','none');
 });
-$(".bt5").click(function(){
+$(".bt5").on('touchend',function(){
     $(".bg3").attr('style','visibility:hidden');
     $(".dl5").attr('style','visibility:hidden');
     $(".bt5").attr('style','visibility:hidden');
@@ -49,3 +71,9 @@ $(".bt5").click(function(){
     $(".bg6").attr('style','none');
     $(".bt6").attr('style','none');
 });
+$("#c").on('touchend',function(){
+   window.location= ('../html/L2.html')
+});
+$("#d").on('touchend',function(){
+    window.location= ('../html/L2.html')
+ });
--- js/L2.js
+++ js/L2.js
@@ -1,20 +1,91 @@
-$(".bt7").click(function(){
+var correct = 0
+$(".op7").click(function(){
+    window.location=('../html/ar_cloth.html')
+});
+$("#a1").click(function(){
+    $("#a1").attr('style','visibility:hidden');
+    $("#b1-t").attr('style','visibility:hidden');
+
+    $("#a1-t").attr('style','none');
+    $("#b1").attr('style','none');
+    correct=0;
+    
+});
+$("#b1").click(function(){
+    $("#b1").attr('style','visibility:hidden');
+    $("#a1-t").attr('style','visibility:hidden');
+
+    $("#b1-t").attr('style','none');
+    $("#a1").attr('style','none');
+    correct=1;
+    
+});
+$("#a1-t").click(function(){
+    $("#a1-t").attr('style','visibility:hidden');
+    $("#a1").attr('style','none');
+    correct=0; 
+});
+$("#b1-t").click(function(){
+    $("#b1-t").attr('style','visibility:hidden');
+    $("#b1").attr('style','none');
+    correct=0; 
+});
+
+
+$("#a").on('touchstart click',function(){
+    $("#a").attr('style','opacity: 0.5');
+}); 
+$("#b").on('touchstart click',function(){
+    $("#b").attr('style','opacity: 0.5');
+}); 
+$("#c").on('touchstart click',function(){
+    $("#c").attr('style','opacity: 0.5');
+}); 
+$(".bt11").on('touchstart click',function(){
+    $(".bt11").attr('style','opacity: 0.5');
+}); 
+$(".bt12").on('touchstart click',function(){
+    $(".bt12").attr('style','opacity: 0.5');
+}); 
+$(".bt13").on('touchstart click',function(){
+    $(".bt13").attr('style','opacity: 0.5');
+}); 
+$(".bt14").on('touchstart click',function(){
+    $(".bt14").attr('style','opacity: 0.5');
+}); 
+
+
+$("#c").on('touchend',function(){
+    if(correct==1){
     $(".bg7").attr('style','visibility:hidden');
     $(".op7").attr('style','visibility:hidden');
     $(".bt7").attr('style','visibility:hidden');
 
     $(".bg9").attr('style','none');
-    $(".bt9").attr('style','none');
+    $(".dl9").attr('style','none');
+    $(".bt9").attr('style','none');}
+    else{
+    $("#a1-t").attr('style','visibility:hidden');
+    $("#b1-t").attr('style','visibility:hidden');
+    $("#a1").attr('style','none');
+    $("#b1").attr('style','none');
+    $("#c").attr('style','opacity:0');
+
+    }
+
 });
-$(".bt9").click(function(){
+$(".bt9").on('touchend',function(){
     $(".bg9").attr('style','visibility:hidden');
+    $(".dl9").attr('style','visibility:hidden');
     $(".bt9").attr('style','visibility:hidden');
 
     $(".bg10").attr('style','none');
     $(".dl10").attr('style','none');
     $(".bt10").attr('style','none');
+    
 });
-$(".bt10").click(function(){
+
+$(".bt10").on('touchend',function(){
     $(".bg10").attr('style','visibility:hidden');
     $(".dl10").attr('style','visibility:hidden');
     $(".bt10").attr('style','visibility:hidden');
@@ -23,7 +94,7 @@ $(".bt10").click(function(){
     $(".bt11").attr('style','none');
     
 });
-$(".bt11").click(function(){
+$(".bt11").on('touchend',function(){
     $(".bg11").attr('style','visibility:hidden');
     $(".bt11").attr('style','visibility:hidden');
 
@@ -31,17 +102,20 @@ $(".bt11").click(function(){
     $(".dl12").attr('style','none');
     $(".bt12").attr('style','none');
 });
-$(".bt12").click(function(){
+$(".bt12").on('touchend',function(){
     $(".dl12").attr('style','visibility:hidden');
     $(".bt12").attr('style','visibility:hidden');
 
     $(".dl13").attr('style','none');
     $(".bt13").attr('style','none');
 });
-$(".bt13").click(function(){
+$(".bt13").on('touchend',function(){
     $(".dl13").attr('style','visibility:hidden');
     $(".bt13").attr('style','visibility:hidden');
 
     $(".dl14").attr('style','none');
     $(".bt14").attr('style','none');
+});
+$(".bt14").on('touchend',function(){
+    window.location=("../html/L3.html")
 });
