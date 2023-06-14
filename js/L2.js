52704a51f2de982fa330aaf28fe1ec1cb3c04127
Yinwei-Chiu <65745306+Yinwei-Chiu@users.noreply.github.com>
2023-06-15 02:08:15 +0900

    to da wae

--- js/L2.js
+++ js/L2.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var correct = 0
 $(".op7").click(function(){
     window.location=('../html/ar_cloth.html')
@@ -11,11 +12,22 @@ function music(){
         x3.pause();
         x.autoplay = true;
         x.load();
+=======
+$(document).ready()
+
+var playState = 0;
+$(".myAudio").autoplay = true;
+
+function music(){
+    if(playState ===  0) {
+        $(".myAudio").autoplay = true;
+>>>>>>> Stashed changes
         playState = 1;
     }
 }
 document.addEventListener('click', music); 
 
+<<<<<<< Updated upstream
 var x2 = document.getElementById("myAudio_no");
 var playState2 = 0;
 function music2(){
@@ -129,19 +141,74 @@ $(".bt11").on('touchend',function(){
     music2()
 });
 $(".bt12").on('touchend',function(){
+=======
+var answer7 = 0; 
+$(".bt7#a").click(function(){
+    $(".bg7#a").attr('style','visibility:hidden');
+    $(".bg7#a-t").attr('style','none');
+    $(".bg7#b").attr('style','none');
+    $(".bg7#a-t").attr('style','visibility:hidden');
+    answer7 = false;
+});
+$(".bt7#b").click(function(){
+    $(".bg7#b").attr('style','visibility:hidden');
+    $(".bg7#b-t").attr('style','none');
+    $(".bg7#a").attr('style','none');
+    $(".bg7#a-t").attr('style','visibility:hidden');
+    answer7 = true;
+});
+$(".bt7").click(function(){
+    if(answer7 ===1 ){
+        $(".bg7").attr('style','visibility:hidden');
+        $(".bt7").attr('style','visibility:hidden');
+        $(".op7").attr('style','visibility:hidden');
+
+        $(".bg9").attr('style','none');
+        $(".bt9").attr('style','none');
+    }
+    else{
+        $(".bt7#a-t").attr('style','visibility:hidden');
+        $(".bt7#a").attr('style','visibility:none1');
+    }
+});
+$(".bt9").click(function(){
+    $(".bg10").attr('style','visibility:hidden');
+    $(".dl10").attr('style','visibility:hidden');
+    $(".bt10").attr('style','visibility:hidden');
+
+    $(".bt9").attr('style','none');
+    $(".bg9").attr('style','none');
+});
+$(".bt10").click(function(){
+    $(".bg11").attr('style','visibility:hidden');
+    $(".bt11").attr('style','visibility:hidden');
+
+    $(".bg12").attr('style','none');
+    $(".dl12").attr('style','none');
+    $(".bt12").attr('style','none');
+});
+$(".bt12").click(function(){
+>>>>>>> Stashed changes
     $(".dl12").attr('style','visibility:hidden');
     $(".bt12").attr('style','visibility:hidden');
 
     $(".dl13").attr('style','none');
     $(".bt13").attr('style','none');
 });
+<<<<<<< Updated upstream
 $(".bt13").on('touchend',function(){
+=======
+$(".bt13").click(function(){
+>>>>>>> Stashed changes
     $(".dl13").attr('style','visibility:hidden');
     $(".bt13").attr('style','visibility:hidden');
 
     $(".dl14").attr('style','none');
     $(".bt14").attr('style','none');
+<<<<<<< Updated upstream
 });
 $(".bt14").on('touchend',function(){
     window.location=("../html/L3.html")
+=======
+>>>>>>> Stashed changes
 });
