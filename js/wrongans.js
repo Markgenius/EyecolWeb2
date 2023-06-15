22a661e2ae41a1f7268c856e670af01c1c4add5e
Yinwei-Chiu <65745306+Yinwei-Chiu@users.noreply.github.com>
2023-06-15 12:55:11 -0400

    qq

--- js/wrongans.js
+++ js/wrongans.js
@@ -1,10 +1,11 @@
 var x = document.getElementById("myAudio");
 var playState = 0;
 function music(){
-    if(playState ===  0) {
-        x2.pause();
+    if(playState ===  0){
         x.autoplay = true;
         x.load();
         playState = 1;
     }
 }
+
+document.addEventListener('click', music); 
